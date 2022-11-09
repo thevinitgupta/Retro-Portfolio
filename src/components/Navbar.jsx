8cac772814b79029c1578248a22b2a714a301df9
Vinit Gupta <vinitman35@gmail.com>
2022-11-09 17:18:06 +0530

    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,33 @@
-import { HiOutlineMenu } from "react-icons/hi"
+import { useState } from "react"
+import { HiOutlineMenuAlt1, HiOutlineX } from "react-icons/hi"
 import "../styles/Navbar.css"
 
 const Navbar = () =>{
+    const [displayMenu,setDisplayMenu] = useState(false);
+
+    const toggleMenu = () =>{
+        setDisplayMenu((prev) => {
+            return !prev;
+        })
+    }
 
     return(
         <nav className="navbar">
             <div id="logo">thevinigupta</div>
-            <ul className="menu">
+            <ul className="menu menu-lg">
                 <li className="menu-item">Experience</li>
                 <li className="menu-item">Projects</li>
                 <li className="menu-item">About</li>
                 <li className="menu-item">Contact</li>
             </ul>
-            <HiOutlineMenu fontSize={28} id="ham-icon"/>
+            <HiOutlineMenuAlt1 fontSize={36} onClick={toggleMenu} id="ham-icon"/>
+            {displayMenu && <ul className="menu menu-sm">
+                <li className="menu-item">Experience</li>
+                <li className="menu-item">Projects</li>
+                <li className="menu-item">About</li>
+                <li className="menu-item">Contact</li>
+                <HiOutlineX fontSize={32} onClick={toggleMenu} id="cross-icon"/>
+            </ul>}
         </nav>
     )
 }
