f315ccd2e6277efa33a233381f991fd3991cc43b
Vinit Gupta <vinitman35@gmail.com>
2022-11-11 22:09:57 +0530

    make window cards responsive

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -31,16 +31,16 @@ const Experience = () =>{
             </div>
             <div className="experience-body">
                 <div className="experience-section" id="tablt" ref={tabltRef}>
-                    <Window head="Worked as an SDE Intern" body="Boldare Boards helps to make online meetings as good as offline. Minus a pile of sticky notes on the floor, of course. Thanks to Boldare Boards you can: " img="" backgroundColor="--background" color="--primary" height="40vh" minHeight="300px" width="30vw" minWidth="320px" margin="3% 0 0 0" />
+                <Window head="Worked as an SDE Intern" body="Boldare Boards helps to make online meetings as good as offline. Minus a pile of sticky notes on the floor, of course. Thanks to Boldare Boards you can: " img="" backgroundColor="--background" color="--primary" height="40vh" width="420px" margin="0 0 0 0" />
 
-                    <Window head="" body="" img="https://media.glassdoor.com/l/1791038/tablt-office.jpg" backgroundColor="--background" color="--primary" height="42vh" minHeight="300px" width="35vw" minWidth="320px" margin="-7% 0 0 0"/>
+                <Window head="" body="" img="https://media.glassdoor.com/l/1791038/tablt-office.jpg" backgroundColor="--background" color="--primary" height="40vh" width="480px" margin="0 0 0 0"/>
                     <img src={Arrow} onClick={toggleSection}  className="arrow right-arrow" alt="arrow"/>
                 </div>
 
                 <div className="experience-section" id="uptone" ref={uptoneRef}>
-                    <Window head="Worked as an SDE Intern" body="Boldare Boards helps to make online meetings as good as offline. Minus a pile of sticky notes on the floor, of course. Thanks to Boldare Boards you can: " img="" backgroundColor="--background" color="--primary" height="40vh" width="420px" margin="3% 0 0 0" />
+                    <Window head="Worked as an SDE Intern" body="Boldare Boards helps to make online meetings as good as offline. Minus a pile of sticky notes on the floor, of course. Thanks to Boldare Boards you can: " img="" backgroundColor="--background" color="--primary" height="40vh" width="420px" margin="0 0 0 0" />
 
-                    <Window head="" body="" img="https://media.glassdoor.com/l/1791038/tablt-office.jpg" backgroundColor="--background" color="--primary" height="360px" width="480px" margin="-7% 0 0 0"/>
+                    <Window head="" body="" img="https://media.glassdoor.com/l/1791038/tablt-office.jpg" backgroundColor="--background" color="--primary" height="40vh" width="480px" margin="0 0 0 0"/>
                     <img src={Arrow} onClick={toggleSection} className="arrow left-arrow" alt="arrow"/>
                 </div>
                 
