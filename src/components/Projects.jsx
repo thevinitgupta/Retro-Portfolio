4544bff606cff533528f91e2a8a2eaeb8968b9e3
Vinit Gupta <vinitman35@gmail.com>
2023-01-29 12:28:12 +0530

    add projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import {default as Header} from "./Header"
 import "../styles/Projects.css"
 import { useState } from "react"
+import Camera from "../assets/camera.gif"
+import Eth from "../assets/ethereum.gif"
+import Arrow from "../assets/arrow.svg"
 
 const Projects = () =>{
     const [active,setActive] = useState(false);
@@ -10,33 +13,22 @@ const Projects = () =>{
             <Header p1="I Built These" font="--bungee" color="--bright-orange" shadow="#00000085" transform="rotate(-4deg)" margin="0"/>
             </div>
             <div className="projects-body">
-                <div className="card">
-                <div className="front">
+                <div className="card pigshell" style={{backgroundColor : "#F8485E"}}>
                     <div className="front-head">Pigshell</div>
-                    <div className="front-body">
-                        Convert boring images to ASCII based media
-                        <br/>Tech Stack : <br/>ReactJS <br/> Firebase
-                    </div>
-                </div>
-                <div className="back">
-                    <h2></h2>
                     <p>Features include converting images and live video into ASCII based media. Users can also create accounts and upload their pics to access from anywhere</p>
+                    <div className="more-btn">More</div>
+                    <img src={Camera} alt="camera" className="pig-cam" />
                 </div>
-                </div>
-                <div className="card">
-                <div className="front" style={{backgroundColor : "#AC63ED"}}>
-                    <div className="front-head">Pigshell</div>
-                    <div className="front-body">
-                        Convert boring images to ASCII based media
-                        <br/>Tech Stack : <br/>ReactJS <br/> Firebase
-                    </div>
-                </div>
-                <div className="back">
-                    <h2></h2>
-                    <p>Features include converting images and live video into ASCII based media. Users can also create accounts and upload their pics to access from anywhere</p>
-                </div>
+                <div className="card zuros" style={{backgroundColor : "#AC63ED"}}>
+                <div className="front-head">Zuros</div>
+                    <p>A Decentralized Crypto exchange application built on the Ethereum blockchain. Users can send messages and GIFs during exchange</p>
+                    <div className="more-btn">More</div>
+                    <img src={Eth} alt="camera" className="eth-block" />
                 </div>
                 
+            </div>
+            <div className="projects-all">
+                See All <img src={Arrow} className="arrow" alt="arrow"/>
             </div>
         </div>
     )
